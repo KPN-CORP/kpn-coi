329d527d6f8c4a0b51cdb4691a3e096c724a92d9
Fredian Simanjuntak KPN <fredian.simanjuntak@kpn-corp.com>
2026-07-15 18:48:49 +0700

    add public view

---
+++ public/build/assets/EmployeeLayout-dZB330eP.js
@@ -0,0 +1,5 @@
+<<<<<<<< HEAD:public/build/assets/EmployeeLayout-B5iJV3rX.js
+import{F as e,G as t,N as n,R as r,T as i,y as a}from"./app-C1ZY9gQl.js";import{i as o,n as s}from"./navigation-BS81zV-0.js";var c=i({__name:`EmployeeLayout`,setup(i){let c={name:`Metta Saputra`,employee_id:`KPN-01124040`,initials:`M`};return(i,l)=>(n(),a(o,{navigation:t(s),user:c},{default:r(()=>[e(i.$slots,`default`)]),_:3},8,[`navigation`]))}});export{c as t};
+========
+import{F as e,G as t,N as n,R as r,T as i,y as a}from"./app-BgzS1lMg.js";import{i as o,n as s}from"./navigation-BEwPUQTO.js";var c=i({__name:`EmployeeLayout`,setup(i){let c={name:`Metta Saputra`,employee_id:`KPN-01124040`,initials:`M`};return(i,l)=>(n(),a(o,{navigation:t(s),user:c},{default:r(()=>[e(i.$slots,`default`)]),_:3},8,[`navigation`]))}});export{c as t};
+>>>>>>>> fixes:public/build/assets/EmployeeLayout-dZB330eP.js
