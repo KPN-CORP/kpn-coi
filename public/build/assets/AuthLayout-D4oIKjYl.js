6153048d55496209ef7ef014a7c097ac9def6db6
Fredian Simanjuntak KPN <fredian.simanjuntak@kpn-corp.com>
2026-07-21 10:00:50 +0700

    add build public

--- public/build/assets/AuthLayout-D4oIKjYl.js
+++
@@ -1,1 +0,0 @@
-import{F as e,N as t,v as n,x as r}from"./app-VtdQ8GfZ.js";import{t as i}from"./_plugin-vue_export-helper-BDNMzG2s.js";var a={},o={class:`flex min-h-screen items-center justify-center bg-slate-100 px-4`},s={class:`w-full max-w-md rounded-xl bg-white p-8 shadow-sm`};function c(i,a){return t(),r(`div`,o,[n(`div`,s,[e(i.$slots,`default`)])])}var l=i(a,[[`render`,c]]);export{l as t};
